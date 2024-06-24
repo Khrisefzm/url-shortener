06e30108a553d760d61133be3685fe5330326435
khrisefzm <khrisefzm@gmail.com>
2024-06-24 12:48:37 -0500

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,13 @@ const Navbar = () => {
     <nav className="bg-gray-800 w-full">
       <div className="container p-4 mx-auto flex justify-between">
         <Link to="/" className="text-white text-xl">
-          URL Shortener
+          <img
+            src="./logo.png"
+            alt="logo"
+            width={60}
+            height={60}
+            loading="lazy"
+          />
         </Link>
         <div>
           <Link to="/" className="text-gray-300 mx-2">
